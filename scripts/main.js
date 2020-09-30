1920c12d3c1a3430cf09a532354885546baebcef
Sereda Vlad <seredavlad40@gmail.com>
2020-09-30 23:51:27 +0300

    da1 1 done

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
+'use strict';
+
 const btnBurder = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
 const btnClose = document.querySelector('.btn-close');
-const overlay = document.querySelector('.overlay');
+
 
 const subCatalog = document.querySelector('.subcatalog');
 const subcatalogHeader = document.querySelector('.subcatalog-header');
+const btnReturn = document.querySelector('.btn-return');
+
+const overlay = document.createElement('div');
+overlay.className = 'overlay';
+document.body.insertAdjacentElement('beforeend',overlay);
+
+
 const openMenu = () =>{
     catalog.classList.add('open');
     overlay.classList.add('active')
@@ -12,20 +21,27 @@ const openMenu = () =>{
 
 const closeMenu = () =>{
     catalog.classList.remove('open');
-    overlay.classList.remove('active')
+    overlay.classList.remove('active');
+    closeSubMenu();
 };
 
 const openSubMenu = event =>{
     event.preventDefault();
-    const target = event.target;
-    const itemList = target.closest('.catalog-list__item');
+    
+    const itemList = event.target.closest('.catalog-list__item');
     if(itemList) {
-        subcatalogHeader.innerHTML = itemList.innerHTML;
+        subcatalogHeader.innerHTML = itemList.innerHTML
         
         subCatalog.classList.add('subopen');
     }
 }
 
+const closeSubMenu = () => {
+    subCatalog.classList.remove('subopen');
+}
+
 btnBurder.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
 catalog.addEventListener('click', openSubMenu);
+btnReturn.addEventListener('click', closeSubMenu);
+overlay.addEventListener('click', closeMenu)
